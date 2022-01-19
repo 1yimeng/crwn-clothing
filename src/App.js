b85653b35a604b88f246f699b66cb5e302cd1c9a
Yi Meng Wang <ywang25@Yis-MacBook-Pro.local>
2022-01-19 14:50:30 -0700

    finished collection preview and item plus style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.components';
+import { Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+
+
 function App() {
   return (
-    <HomePage />
+    <div>
+      <switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </switch>
+    </div>
   );
 }
 
